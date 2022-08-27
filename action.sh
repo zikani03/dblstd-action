f9ecf18f3b7efe5c6bc9bdce399a09008f729d3d
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2022-08-28 00:52:23 +0200

    Remove action.sh since steps now inlined in action.yaml

--- action.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git clone https://github.com/sethbonnie/dblstd dblstd-src
-cd ./dblstd-src
-go build -o dblstd . && chmod +x ./dblstd
-cd ..
-./dblstd-src/dblstd -s "${{ inputs.shapeFile }}" .
