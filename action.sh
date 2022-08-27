1785eb83936c3455675504c36d2b482d167206a8
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2022-08-28 00:46:54 +0200

    use input.shapeFile

--- action.sh
+++ action.sh
@@ -3,4 +3,4 @@ git clone https://github.com/sethbonnie/dblstd dblstd-src
 cd ./dblstd-src
 go build -o dblstd . && chmod +x ./dblstd
 cd ..
-./dblstd-src/dblstd -s $INPUT_SHAPEFILE .
+./dblstd-src/dblstd -s "${{ inputs.shapeFile }}" .
